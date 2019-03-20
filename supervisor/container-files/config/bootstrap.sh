d7296223f4ff39ef805082379e15009d90df405f
Vincent <huangqinglong@hivision-info.cn>
2019-03-20 21:36:36 +0800

    update supervisord

--- supervisor/container-files/config/bootstrap.sh
+++ supervisor/container-files/config/bootstrap.sh
@@ -10,7 +10,7 @@ SUPERVISOR_PARAMS='-c /etc/supervisord.conf'
 # Create directories for supervisor's UNIX socket and logs (which might be missing
 # as container might start with /data mounted from another data-container).
 mkdir -p /data/conf /data/run /data/logs /data/conf/supervisor.d
-chmod 711 /data/conf /data/run /data/logs /data/conf/supervisor.d
+chmod 755 /data/conf /data/run /data/logs /data/conf/supervisor.d
 
 if [ "$(ls /config/init/)" ]; then
   for init in /config/init/*.sh; do
